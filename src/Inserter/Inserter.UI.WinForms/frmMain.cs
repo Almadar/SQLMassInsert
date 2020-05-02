2fb866862d2820bf94bdf4b3d57c7fe59f0479d3
Emmanuel Medina <emmanuel.medina@dudesolutions.com>
2020-05-02 17:23:59 -0600

    Recreated the Logic Project
    Recreated the Logic Project to work with .NET Framework

---
+++ src/Inserter/Inserter.UI.WinForms/frmMain.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Inserter.UI.WinForms
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
