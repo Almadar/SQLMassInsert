05958d99234a931b3cb20d0cb17033474d384718
Emmanuel Medina <emmanuel.medina@dudesolutions.com>
2020-05-03 00:55:06 -0600

    Modified Structure
    Modified the structure of the solution, added Constants and DatabaseObjectExplorer

---
+++ src/Inserter/Inserter.Logic/Database/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace Inserter.Logic.Database
+{
+    public static class Constants
+    {
+        public static readonly string QUERY_ALL_DATABASES = "SELECT name FROM sys.databases";
+        public static readonly string QUERY_ALL_TABLES = "SELECT name FROM sys.tables";
+        public static readonly string QUERY_ALL_COLUMNS = "SELECT name FROM sys.columns WHERE object_id = (SELECT object_id FROM sys.tables WHERE name = '{0}'";
+    }
+}
--- src/Inserter/Inserter.Logic/Database/DatabaseObjectExplorer.cs
+++ src/Inserter/Inserter.Logic/Database/DatabaseObjectExplorer.cs
@@ -1,12 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
 
 namespace Inserter.Logic.Database
 {
-    class DatabaseObjectExplorer
+    public class DatabaseObjectExplorer
     {
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+        private string UserName { get; set; }
+        private string Password { get; set; }
+        
+        public List<string> DatabaseList { get; set; }
+        public List<string> TableList { get; set; }
+        public List<string> ColumnList { get; set; }
+        public List<string> ViewList { get; set; }
+        public List<string> StoredProcedureList { get; set; }
+        
+        private string connectionString;
+        private SqlConnection connection;           
+
+        public DatabaseObjectExplorer(string server, string db)
+        {
+            ServerName = server;
+            DatabaseName = db;
+
+            connectionString = $"Server={ServerName}, Database={DatabaseName}, Trusted Connection=True";
+            connection = new SqlConnection(connectionString);
+
+            InstanceObjects();
+        }
+
+        public DatabaseObjectExplorer(string server, string db, string user, string pwd)
+        {
+            ServerName = server;
+            DatabaseName = db;
+            UserName = user;
+            Password = pwd;
+
+            connectionString = $"Server={ServerName}, Database={DatabaseName}, User Id={UserName}, Password={Password}";
+            connection = new SqlConnection(connectionString);
+
+            InstanceObjects();
+        }
+
+        private void InstanceObjects()
+        {
+            DatabaseList = new List<string>();
+            TableList = new List<string>();
+            ColumnList = new List<string>();
+            ViewList = new List<string>();
+            StoredProcedureList = new List<string>();
+        }
+
+        public List<string> GetAllDatabaseNames()
+        {
+            var databases = new List<string>();
+
+            try
+            {
+                connection.Open();
+                connection.Close();
+            }
+            catch
+            {
+                throw;
+            }
+
+            return databases;
+        }
     }
 }
